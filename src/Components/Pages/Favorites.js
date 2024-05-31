a1d74efe02411482c4c811c3dd7b2e22928219d6
Koswi <djmaglaqui@gmail.com>
2024-05-31 13:57:36 +0800

    koswi

--- src/Components/Pages/Favorites.js
+++ src/Components/Pages/Favorites.js
@@ -52,6 +52,7 @@ const Favorites = () => {
                 setUserLoggedData(JSON.parse(storedProfileData))
             }
         }
+
         fetchUserProfile();
         fetchFavoriteProducts(setProductDetails, setLoadingProducts);
     }, []);
@@ -81,6 +82,56 @@ const Favorites = () => {
     };
 
 
+    const renderFavoriteProducts = () => {
+        if (productDetails.length){
+            return (
+                <>
+                    {!loadingProducts ? <>
+                        {productDetails.map((favorite, i) => (
+                            <div className="fcpcMi1Content" key={i}>
+                                <div className="fcpcm1cGamePlatform">
+                                    <img src='' platform={favorite.productData.game_platform} alt="" />
+                                </div>
+                                <Link to={`/Games/${favorite.productData.game_canonical}`}>{favorite.productData.game_cover !== '' ?
+                                <img src={`https://2wave.io/GameCovers/${favorite.productData.game_cover}`} alt="Image Not Available" />
+                                :<img src={require('../assets/imgs/GameBanners/DefaultNoBanner.png')} />}</Link>
+                                <div className="fcpcm1cGameDetails">
+                                    <h5>{favorite.productData.game_title}</h5>
+                                    <p>{favorite.productData.game_edition}</p>
+                                    <div>
+                                        <div id="mppcm2GDView"><h5>$999.99</h5></div>
+                                        <button id='mppcm2GDHeart' onClick={() => handleRemoveFavorite(favorite)}><TbHeartFilled className='faIcons'/></button>
+                                        <button id='mppcm2GDCart'><TbShoppingCartBolt className='faIcons'/></button>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}</>:<>
+                            <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
+                            <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
+                            <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
+                            <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
+                            <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
+                    </>}
+                </>
+            );
+        } else {
+            return (
+                <>
+                    {!loadingProducts ? <>
+                    <div className="fcpcMid1ContainerEmpty">
+                        <h6>No Liked Products</h6>
+                    </div></>:<>
+                        <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
+                        <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
+                        <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
+                        <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
+                        <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
+                    </>}
+                </>
+            );
+        }
+    };
+
 
     return (
         <div className='mainContainer favorites'>
@@ -99,32 +150,7 @@ const Favorites = () => {
             </section>
             <section className="favPageContainer mid">
                 <div className="fcpcMid1Container">
-                    {!loadingProducts ? <>
-                    {productDetails.map((favorite, i) => (
-                        <div className="fcpcMi1Content" key={i}>
-                            <div className="fcpcm1cGamePlatform">
-                                <img src='' platform={favorite.productData.game_platform} alt="" />
-                            </div>
-                            <Link to={`/Games/${favorite.productData.game_canonical}`}>{favorite.productData.game_cover !== '' ?
-                            <img src={`https://2wave.io/GameCovers/${favorite.productData.game_cover}`} alt="Image Not Available" />
-                            :<img src={require('../assets/imgs/GameBanners/DefaultNoBanner.png')} />}</Link>
-                            <div className="fcpcm1cGameDetails">
-                                <h5>{favorite.productData.game_title}</h5>
-                                <p>{favorite.productData.game_edition}</p>
-                                <div>
-                                    <div id="mppcm2GDView"><h5>$999.99</h5></div>
-                                    <button id='mppcm2GDHeart' onClick={() => handleRemoveFavorite(favorite)}><TbHeartFilled className='faIcons'/></button>
-                                    <button id='mppcm2GDCart'><TbShoppingCartBolt className='faIcons'/></button>
-                                </div>
-                            </div>
-                        </div>
-                    ))}</>:<>
-                        <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
-                        <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
-                        <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
-                        <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
-                        <div className="fcpcMi1CDummy"><div className="fcpcm1cGPDummy"></div></div>
-                    </>}
+                    {renderFavoriteProducts()}
                 </div>
             </section>
         </div>
