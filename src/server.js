522a18b5a338d14f02fec463f88146c28233bea5
hope <maglaquijohnhopefuedan@gmail.com>
2024-06-19 16:20:05 +0800

    paypal

--- src/server.js
+++ src/server.js
@@ -5,71 +5,155 @@ const stripe = require("stripe")(
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+
+const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 8888 } = process.env;
+const base = "https://api-m.sandbox.paypal.com";
 const app = express();
 
+// host static files
+app.use(express.static("client"));
+
+// parse post params sent in body in json format
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 
-// Function to calculate total unit amount
-const calculateTotalUnitAmount = (lineItems) => {
-  let totalAmount = 0;
+/**
+ * Generate an OAuth 2.0 access token for authenticating with PayPal REST APIs.
+ * @see https://developer.paypal.com/api/rest/authentication/
+ */
+const generateAccessToken = async () => {
+  try {
+    if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+      throw new Error("MISSING_API_CREDENTIALS");
+    }
+    const auth = Buffer.from(
+      PAYPAL_CLIENT_ID + ":" + PAYPAL_CLIENT_SECRET
+    ).toString("base64");
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+      method: "POST",
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
 
-  lineItems.forEach((item) => {
-    totalAmount += item.price_data.unit_amount;
-  });
-
-  return totalAmount;
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error("Failed to generate Access Token:", error);
+  }
 };
 
-app.post("/create-check-out-session", async (req, res) => {
-  const { product } = req.body;
-
-  
-  const line_items = product.map((item) => ({
-    price_data: {
-      currency: "usd",
-      product_data: {
-        name: item.ag_product_name,
-        images: [item.productData.game_cover],
-      },
-      unit_amount: Math.round(item.totalPrice * 100), // Convert to cents
-    },
-    quantity: item.numberOfOrder,
-  }));
-
-  const totalUnitAmount = calculateTotalUnitAmount(line_items);
-  const agChargeFee = ((4.5 / 100) * totalUnitAmount);
-  const agTaxFee = ((3 / 100) * totalUnitAmount)
-  const totalPayable = totalUnitAmount + agChargeFee + agTaxFee;
-  console.log(totalPayable);
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: parseInt(totalPayable),
-    currency: "usd",
-  });
-  res.send({
-    paymentIntentID: paymentIntent.id,
-    clientSecret: paymentIntent.client_secret,
-    line_items: line_items,
-  });
-});
-
-app.post("/cancel-payment-intent", async (req, res) => {
-  const { paymentIntentId } = req.body;
-
+async function handleResponse(response) {
   try {
-    const canceledPaymentIntent = await stripe.paymentIntents.cancel(
-      paymentIntentId
-    );
-    console.log(canceledPaymentIntent);
-    res.status(200).send(canceledPaymentIntent);
-  } catch (error) {
-    res.status(500).send({ error: error.message });
+    const jsonResponse = await response.json();
+    return {
+      jsonResponse,
+      httpStatusCode: response.status,
+    };
+  } catch (err) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
   }
+}
+
+/**
+ * Create an order to start the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
+ */
+const createOrder = async (cart) => {
+  // use the cart information passed from the front-end to calculate the purchase unit details
+  console.log(
+    "shopping cart information passed from the frontend createOrder() callback:",
+    cart
+  );
+
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders`;
+
+  const payload = {
+    intent: "CAPTURE",
+    purchase_units: [
+      {
+        amount: {
+          currency_code: "USD",
+          value: "100",
+        },
+      },
+    ],
+  };
+  
+
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+      // Uncomment one of these to force an error for negative testing (in sandbox mode only).
+      // Documentation: https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+      // "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    },
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  return handleResponse(response);
+};
+
+// createOrder route
+app.post("/api/orders", async (req, res) => {
+  try {
+    // use the cart information passed from the front-end to calculate the order amount detals
+    const { cart } = req.body;
+    const { jsonResponse, httpStatusCode } = await createOrder(cart);
+    res.status(httpStatusCode).json(jsonResponse);
+  } catch (error) {
+    console.error("Failed to create order:", error);
+    res.status(500).json({ error: "Failed to create order." });
+  }
+}); 
+/**
+ * Capture payment for the created order to complete the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_capture
+ */
+const captureOrder = async (orderID) => {
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders/${orderID}/capture`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+      // Uncomment one of these to force an error for negative testing (in sandbox mode only).
+      // Documentation:
+      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    },
+  });
+
+  return handleResponse(response);
+};
+
+// captureOrder route
+app.post("/api/orders/:orderID/capture", async (req, res) => {
+  try {
+    const { orderID } = req.params;
+    const { jsonResponse, httpStatusCode } = await captureOrder(orderID);
+    res.status(httpStatusCode).json(jsonResponse);
+  } catch (error) {
+    console.error("Failed to create order:", error);
+    res.status(500).json({ error: "Failed to capture order." });
+  }
+}); // serve index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./checkout.html"));
 });
 
-
-app.get("/success", (req, res) => {
-  res.send("payment successfull");
-});
-
-app.listen(4242, () => console.log("Node server listening on port 4242!"));
+app.listen(PORT, () => {
+  console.log(`Node server listening at http://localhost:${PORT}/`);
+}); 
