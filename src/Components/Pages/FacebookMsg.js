6e320f5a6e2572ee2f1000ce48dd4e9ee82600ec
Koswi <djmaglaqui@gmail.com>
2024-07-03 15:48:58 +0800

    koswi

---
+++ src/Components/Pages/FacebookMsg.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FacebookProvider, CustomChat } from 'react-facebook';
+
+const FacebookMsg = () => {
+  return (
+    <FacebookProvider appId="1191357838684307" chatSupport>
+        <CustomChat pageId="149440418245234" minimized={true}/>
+    </FacebookProvider>  
+  )
+}
+
+export default FacebookMsg
